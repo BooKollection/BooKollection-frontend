27551b84696f84c56d6eba1da0afc7dcb3fa83be
anagdinizp <anagabriela0399@hotmail.com>
2022-04-27 11:10:26 -0300

    Adding vLibras to the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import { storeWrapper } from '../store'
 import { Navbar } from '../components/molecules/navbar'
 import '../styles/globals.css'
 import { theme } from '../styles/theme'
+import VLibras from '@djpfs/react-vlibras';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <Navbar>
         <Component {...pageProps} />
+        <VLibras/>
       </Navbar>
     </ThemeProvider>
   )
