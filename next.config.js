44535dfddc2a9cd6a323a7d11de8e1e510f35b9d
DiegoAmancio <diego.pereira@ccc.ufcg.edu.br>
2022-07-27 20:40:53 -0300

    fix: next.config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const webpack = require('webpack')
-
-const { parsed: myEnv } = require('dotenv').config({
-  path: '.env'
-})
-console.log(myEnv.dev)
+const result = require('dotenv').config()
 module.exports = {
+  env: result.parsed,
   images: {
     domains: ['images-na.ssl-images-amazon.com', 'i.imgur.com']
   },
-  reactStrictMode: myEnv.dev === 'true',
+  reactStrictMode: false,
   i18n: {
     locales: ['pt-BR', 'en-US', 'fr-FR', 'es-ES'],
     defaultLocale: 'en-US'
-  },
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
-    return config
   }
 }
